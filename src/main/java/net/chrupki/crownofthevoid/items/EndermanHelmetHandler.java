8739072bcc2dea4b942d45efa1893be3db7cbb66
jakubaszcz <jakub.szczucinski@epitech.eu>
2025-09-17 21:13:41 +0200

    ADD: Handling enderman with helmet

---
+++ src/main/java/net/chrupki/crownofthevoid/items/EndermanHelmetHandler.java
@@ -0,0 +1,18 @@
+package net.chrupki.crownofthevoid.items;
+
+import net.chrupki.crownofthevoid.CrownOfTheVoid;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraftforge.event.entity.living.EnderManAngerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = CrownOfTheVoid.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class EndermanHelmetHandler {
+    @SubscribeEvent
+    public static void onEndermanAnger(EnderManAngerEvent event) {
+        var player = event.getPlayer();
+        if (player != null && player.getItemBySlot(EquipmentSlot.HEAD).is(Items.ENDERITE_HELMET.get())) {
+            event.setCanceled(true);
+        }
+    }
+}
